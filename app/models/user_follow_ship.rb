7189557b7ac2fcef77f1d8aa3f6466fd18b27645
Ray <ray9015@gmail.com>
2023-06-04 00:22:45 +0800

    :bulb: generate model summary

--- app/models/user_follow_ship.rb
+++ app/models/user_follow_ship.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: user_follow_ships
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  follow_id  :bigint
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_user_follow_ships_on_follow_id  (follow_id)
+#  index_user_follow_ships_on_user_id    (user_id)
+#
 class UserFollowShip < ApplicationRecord
 end
--- spec/models/user_follow_ship_spec.rb
+++ spec/models/user_follow_ship_spec.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: user_follow_ships
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  follow_id  :bigint
+#  user_id    :bigint
+#
+# Indexes
+#
+#  index_user_follow_ships_on_follow_id  (follow_id)
+#  index_user_follow_ships_on_user_id    (user_id)
+#
 require 'rails_helper'
 
 RSpec.describe UserFollowShip, type: :model do
