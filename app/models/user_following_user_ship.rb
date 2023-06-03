a251dbebc3339b65ca2eedbf37f87d5f0c5cdeca
Ray <ray9015@gmail.com>
2023-06-04 03:08:34 +0800

    :art: add validator to prevent self & duplicate user-following relationships

--- app/models/user_following_user_ship.rb
+++ app/models/user_following_user_ship.rb
@@ -10,10 +10,20 @@
 #
 # Indexes
 #
+#  index_on_user_id_and_following_user_id                (user_id,following_user_id) UNIQUE
 #  index_user_following_user_ships_on_following_user_id  (following_user_id)
 #  index_user_following_user_ships_on_user_id            (user_id)
 #
 class UserFollowingUserShip < ApplicationRecord
+  validates :user, uniqueness: { scope: :following_user }
+  validate :cannot_follow_yourself
+
   belongs_to :user
   belongs_to :following_user, class_name: 'User'
+
+  private
+
+  def cannot_follow_yourself
+    errors.add(:user_id, 'cannot follow yourself') if user_id == following_user_id
+  end
 end
