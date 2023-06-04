1c0a54d79ca9fcbb40584a8e6e66ca92fedee982
Ray <ray9015@gmail.com>
2023-06-04 17:26:52 +0800

    :recycle: extract general API method to api controller

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,6 +1,25 @@
 module Api
   module V1
     class ApiController < ApplicationController
+      # @param [Object] response_data
+      def response_success(response_data)
+        api_response.data = response_data
+
+        render json: api_response.to_response
+      end
+
+      def response_error(status_code, error_message)
+        api_response.set_error(status_code, error_message)
+
+        render json: api_response.to_response
+      end
+
+      private
+
+      # @return [Api::V1::ApiResponse]
+      def api_response
+        @api_response ||= ApiResponse.new
+      end
     end
   end
 end
