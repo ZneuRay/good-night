ec7e49b05f0a04671afa31d626f749e8bfbec3c2
Ray <ray9015@gmail.com>
2023-06-04 19:47:34 +0800

    :sparkles: add API to list user's sleep diary

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: %i[index show create]
+      resources :users, only: %i[index show create] do
+        resources :user_sleep_diary, only: %i[index show] do
+        end
+      end
     end
   end
 end
