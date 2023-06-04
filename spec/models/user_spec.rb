31f369974c8f363f4d9fafab5681c7bb1b72d583
Ray <ray9015@gmail.com>
2023-06-04 15:13:33 +0800

    :white_check_mark: add test for the user follow functionality

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,8 +17,26 @@ RSpec.describe User, type: :model do
     expect(subject).to be_valid
   end
 
-  it 'is  not valid without a name' do
+  it 'is not valid without a name' do
     subject.name = nil
     expect(subject).to be_invalid
+  end
+
+  let(:user1) { create(:user) }
+  let(:user2) { create(:user) }
+
+  it 'is valid for user1 follow user2' do
+    user1.follow(user2)
+    expect(user1.following_users).to include(user2)
+    expect(user2.followers).to include(user1)
+  end
+
+  it 'is not valid to follow a user twice' do
+    user1.follow(user2)
+    expect(user1.follow(user2)).to be(false)
+  end
+
+  it 'is not valid to follow self' do
+    expect(user1.follow(user1)).to be(false)
   end
 end
