c35570f66b5e604af46b929864f72ec8357158fb
Ray <ray9015@gmail.com>
2023-06-04 20:48:47 +0800

    :recycle: extract shared method `user check`

--- app/controllers/api/v1/user_sleep_diary_controller.rb
+++ app/controllers/api/v1/user_sleep_diary_controller.rb
@@ -1,11 +1,16 @@
 module Api
   module V1
     class UserSleepDiaryController < ApiController
+      before_action :check_user
       def index
-        user = User.find_by(id: params[:user_id])
-        response_error(ApiCode::RECORD_NOT_FOUND, 'user not found') and return if user.nil?
+        response_success(Entities::UserSleepDiariesEntity.new(@user.user_sleep_meta.user_sleep_diaries))
+      end
 
-        response_success(Entities::UserSleepDiariesEntity.new(user.user_sleep_meta.user_sleep_diaries))
+      private
+
+      def check_user
+        @user = User.find_by(id: params[:user_id])
+        response_error(ApiCode::RECORD_NOT_FOUND, 'user not found') and return if @user.nil?
       end
     end
   end
