8bb01ecb2367d684de0134211f7301d2bd6f3e77
Ray <ray9015@gmail.com>
2023-06-04 16:22:07 +0800

    :art: add api params validator (DTO) Implement API parameter validator similar to a Data Transfer Object(DTO)

---
+++ app/lib/api/v1/api_params_validate_base.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module Api::V1
+  class ApiParamsValidateBase
+    include ActiveModel::Validations
+
+    # @param [ActionController::Parameters] params
+    def initialize(params = ActionController::Parameters.new)
+      init_params(params)
+    end
+
+    # @param [ActionController::Parameters] params
+    def init_params(params)
+      raise NotImplementedError, "`#{__method__}` is not implemented"
+    end
+
+    # @return [string]
+    def error_messages
+      errors&.full_messages&.to_sentence
+    end
+  end
+end
