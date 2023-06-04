f2056f4d4975735f6bf1ad60e9f5f14252a25705
Ray <ray9015@gmail.com>
2023-06-04 19:59:19 +0800

    :art: add condition for user not found in list user sleep diary API

--- app/controllers/api/v1/user_sleep_diary_controller.rb
+++ app/controllers/api/v1/user_sleep_diary_controller.rb
@@ -2,7 +2,8 @@ module Api
   module V1
     class UserSleepDiaryController < ApiController
       def index
-        user = User.find(params[:user_id])
+        user = User.find_by(id: params[:user_id])
+        response_error(ApiCode::RECORD_NOT_FOUND, 'user not found') and return if user.nil?
 
         response_success(Entities::UserSleepDiariesEntity.new(user.user_sleep_meta.user_sleep_diaries))
       end
