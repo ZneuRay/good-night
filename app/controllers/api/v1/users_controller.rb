3f91fd534ff94fc4390a0627aed09204d68487ec
Ray <ray9015@gmail.com>
2023-06-04 19:05:30 +0800

    :sparkles: add create user API

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,6 +13,18 @@ module Api
           response_error(ApiCode::RECORD_NOT_FOUND, 'user not found')
         end
       end
+
+      def create
+        user_params = Params::UserParams.new(params)
+        response_error(ApiCode::PARAMS_NOT_VALID, user_params.error_messages) and return if user_params.invalid?
+
+        user = User.new
+        user.name = user_params.name
+        response_error(ApiCode::RECORD_NOT_VALID, user.errors.full_messages.to_sentence) and return if user.invalid?
+
+        user.save
+        response_success(Entities::UserEntity.new(user))
+      end
     end
   end
 end
