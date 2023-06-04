3e1d303dd54486d8854c2e55ac9a8c5c3763c4f7
Ray <ray9015@gmail.com>
2023-06-04 18:03:09 +0800

    :sparkles: add show user API

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,6 +4,15 @@ module Api
       def index
         response_success(Entities::UsersEntity.new(User.all))
       end
+
+      def show
+        user = User.find_by(id: params[:id])
+        if user
+          response_success(Entities::UserEntity.new(user))
+        else
+          response_error(ApiCode::RECORD_NOT_FOUND, 'user not found')
+        end
+      end
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: %i[index]
+      resources :users, only: %i[index show]
     end
   end
 end
